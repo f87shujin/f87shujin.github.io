01766a33f112cc6e6488a1180254ae5c48ef6598
f87shujin <fahdidoto87@gmail.com>
2024-11-01 22:53:01 -0400

    project

--- portfolio/src/JS/Projects.js
+++ portfolio/src/JS/Projects.js
@@ -4,6 +4,20 @@ import '../CSS/Project.css';
 
 function Projects() {
     return(
+<div class="projects-container">
+<h1 class="projects-heading">Projects</h1>  
+<div class="project-section">
+  <div class="project-image">
+    <img src="https://repository-images.githubusercontent.com/624589634/1b51aa7b-c125-4fda-9340-b2edcb9cb48a" alt="Project Landscape"/>
+  </div>
+  <div class="project-content">
+    <h2 class="project-title">Project Title</h2>
+    <a href="https://your-project-link.com" class="project-link" aria-label="Visit project">
+      <i class="fa fa-external-link" aria-hidden="true"></i>
+    </a>
+  </div>
+</div>
+
 <div class="project-section">
   <div class="project-image">
     <img src="https://images7.alphacoders.com/132/1323803.jpeg" alt="Project Landscape"/>
@@ -15,6 +29,33 @@ function Projects() {
     </a>
   </div>
 </div>
+
+<div class="project-section">
+  <div class="project-image">
+    <img src="https://images7.alphacoders.com/132/1323803.jpeg" alt="Project Landscape"/>
+  </div>
+  <div class="project-content">
+    <h2 class="project-title">Project Title</h2>
+    <a href="https://your-project-link.com" class="project-link" aria-label="Visit project">
+      <i class="fa fa-external-link" aria-hidden="true"></i>
+    </a>
+  </div>
+</div>
+
+<div class="project-section">
+  <div class="project-image">
+    <img src="https://images7.alphacoders.com/132/1323803.jpeg" alt="Project Landscape"/>
+  </div>
+  <div class="project-content">
+    <h2 class="project-title">Project Title</h2>
+    <a href="https://your-project-link.com" class="project-link" aria-label="Visit project">
+      <i class="fa fa-external-link" aria-hidden="true"></i>
+    </a>
+  </div>
+</div>
+</div>
+
+
 )}
 
 export default Projects;
